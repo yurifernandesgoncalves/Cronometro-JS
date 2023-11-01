c6f1de8e4603bab2c9ce0bbebbd12a8b076ca17a
Yuri Fernandes Goncalves <yurifernandesgoncalves@gmail.com>
2023-11-01 18:45:42 -0300

    finalizando projeto

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ const minutesEl = document.querySelector("#minutes");
 const secondsEl = document.querySelector("#seconds");
 const milisecondsEl = document.querySelector("#miliseconds");
 const startBtn = document.querySelector("#startBtn");
-const pauseBtnE1 = document.querySelector("#pauseBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
 const resumeBtn = document.querySelector("#resumeBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
@@ -13,6 +13,9 @@ let miliseconds = 0;
 let isPaused = false;
 
 startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
 
 function startTimer(){
     interval = setInterval(() => {
@@ -35,12 +38,42 @@ function startTimer(){
             milisecondsEl.textContent = formatMiliseconds(miliseconds);
         };
     }, 10);
+
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block"
 };
+
+function pauseTimer(){
+    isPaused = true;
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "block";
+};
+
+function resumeTimer(){
+    isPaused = false;
+    pauseBtn.style.display ="block";
+    resumeBtn.style.display = "none";
+}
+
+function resetTimer(){
+    clearInterval(interval);
+    minutes = 0;
+    seconds = 0;
+    miliseconds = 0;
+
+    minutesEl.textContent = "00";
+    secondsEl.textContent = "00";
+    milisecondsEl.textContent = "000";
+
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "none";
+}
 
 function formatTimer(time){
     return time < 10 ? `0${time}` : time;
-}
+};
 
 function formatMiliseconds(time){
     return time < 100 ? `${time}`.padStart(3, "0") : time;
-}
+};
